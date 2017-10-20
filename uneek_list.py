04252b259430736fee363c72172ecc46cbe9967a
Ronak Buch <ronakbuch@gmail.com>
2017-10-20 17:55:17 -0500

    Fix issues in uneek_list and remove use of second column
    Change-Id: Ic2bf5b79d7ef123ff7a3a84e07771dce2948c64e

--- uneek_list.py
+++ uneek_list.py
@@ -1,15 +1,19 @@
 #! /usr/bin/env python
 
 import sys
-import hashlib
-import bisect
 
-def inList(a, x):
-    'Locate the leftmost value exactly equal to x'
-    i = bisect.bisect_left(a, x)
-    if i != len(a) and a[i] == x:
-        return True
-    return False
+def inList(genes, value):
+    global counter
+    while counter < len(genes) and genes[counter] < value:
+        counter = counter + 1
+    return counter < len(genes) and genes[counter] == value
+
+def parseLine(line):
+    first, _ = line.split(None, 1)
+    first = first[14:].split('#')[0].split(':')
+    first[2] = first[2].zfill(6)
+
+    return int(''.join(first))
 
 if len(sys.argv) < 3:
     print "You must supply at least two file names: ./uneek.py file1 file2 ..."
@@ -18,15 +22,15 @@ if len(sys.argv) < 3:
 genes = list()
 
 for index in range(1, len(sys.argv)):
+    counter = 0
     with open(sys.argv[index], 'r') as f:
         for line in f:
             if line[0] == '@':
                 continue
-            first, second, _ = line.split(None, 2)
-            second.zfill(3)
-            value = int(first[14:].split('#')[0].replace(':', '') + second)
+            value = parseLine(line)
 
             if index == 1:
                 genes.append(value)
-            elif inList(genes, value):
+            elif not inList(genes, value):
                 sys.stdout.write(line)
+
